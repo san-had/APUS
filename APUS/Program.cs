5f96dade1a46dd230f6bb732473e70fc46425b1c
san-had <hadhazis@gmail.com>
2018-02-18 21:47:10 +0100

    Execution template in Program.cs

--- APUS/Program.cs
+++ APUS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using APUS.Utils;
+using APUS.ViewModel;
 using System;
 using System.Collections.Generic;
 
@@ -22,6 +23,10 @@ namespace APUS
             var dbPresidents = dataAccess.GetDbPresidents();
 
             var presidents = AutoMapper.Mapper.Map<IEnumerable<DataAccess.DbPresident>, IEnumerable<Models.President>>(dbPresidents);
+
+            var presidentViewList = UpdateViewPresidents(presidents);
+
+            RenderPresidentViewList(presidentViewList);
         }
 
         private static void StartUp()
@@ -29,6 +34,39 @@ namespace APUS
             AutoMapper.Mapper.Initialize(cfg => cfg.AddProfile<AutoMapperPresidentProfile>());
 
             AutoMapper.Mapper.AssertConfigurationIsValid();
+        }
+
+        private static IEnumerable<ViewModel.PresidentView> UpdateViewPresidents(IEnumerable<Models.President> presidents)
+        {
+            var presidentViewList = new List<ViewModel.PresidentView>();
+           
+            foreach (var president in presidents)
+            {
+                var viewPresident = new ViewModel.PresidentView();
+
+                viewPresident.FirstName = president.FirstName;
+                viewPresident.LastName = president.LastName;
+                viewPresident.PresidencyRange = GetPresidencyRange(president.TookOffice, president.LeftOffice);
+                viewPresident.NumberOfPresidencyDays = CalculateNumberOfPresidencyDays(president.TookOffice, president.LeftOffice);
+
+                presidentViewList.Add(viewPresident);
+            }
+            return presidentViewList;
+        }
+
+        private static string GetPresidencyRange(DateTime? tookOffice, DateTime? leftOffice)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int CalculateNumberOfPresidencyDays(DateTime? tookOffice, DateTime? leftOffice)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RenderPresidentViewList(IEnumerable<PresidentView> presidentViewList)
+        {
+            throw new NotImplementedException();
         }
     }
 }
