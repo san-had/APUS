70a8b1d4848033c15878125ebcf190167194db82
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-03-05 16:40:25 +0100

    Added parser

---
+++ APUS/Parsers/CsvParser.cs
@@ -0,0 +1,16 @@
+﻿namespace APUS.Parsers
+{
+    using System;
+    using System.Collections.Generic;
+    using APUS.DataAccess;
+    using APUS.Models;
+
+    public class CsvParser : IParser
+    {
+        public IEnumerable<President> Parse(IEnumerable<DbPresident> dbPresidents)
+        {
+            var presidents = AutoMapper.Mapper.Map<IEnumerable<DataAccess.DbPresident>, IEnumerable<Models.President>>(dbPresidents);
+            return presidents;
+        }
+    }
+}
---
+++ APUS/Parsers/IParser.cs
@@ -0,0 +1,9 @@
+﻿namespace APUS.Parsers
+{
+    using System.Collections.Generic;
+
+    public interface IParser
+    {
+        IEnumerable<Models.President> Parse(IEnumerable<DataAccess.DbPresident> dbPresidents);
+    }
+}
