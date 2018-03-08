697714bd1ba6fca28bd101609c835ccd4691d214
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-03-08 09:32:55 +0100

    Added DataLoader

---
+++ APUS/DataLoader/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿namespace APUS.DataLoader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using APUS.DataAccess;
+    using APUS.Models;
+
+    public class DataLoader : IDataLoader
+    {
+        public IEnumerable<President> LoadData(IEnumerable<DbPresident> dbPresidents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ APUS/DataLoader/IDataLoader.cs
@@ -0,0 +1,9 @@
+﻿namespace APUS.DataLoader
+{
+    using System.Collections.Generic;
+
+    public interface IDataLoader
+    {
+        IEnumerable<Models.President> LoadData(IEnumerable<DataAccess.DbPresident> dbPresidents);
+    }
+}
