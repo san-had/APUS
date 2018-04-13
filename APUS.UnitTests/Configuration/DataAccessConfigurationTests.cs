28d4bd7a92ea78448194c190538a8057896503b5
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-04-13 16:10:10 +0200

    Added tests for ReportGenerator

---
+++ APUS.UnitTests/Configuration/DataAccessConfigurationTests.cs
@@ -0,0 +1,35 @@
+﻿namespace APUS.UnitTests.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Unity;
+    using Xunit;
+
+    public class DataAccessConfigurationTests
+    {
+        [Fact]
+        public void DataAccessConfiguration_ConstructorParameterContainerNullReturnsException()
+        {
+            var fakeUnityContainer = new UnityContainer();
+        }
+
+        [Fact]
+        public void DataAccessConfiguration_ConstructorParameterFileNameNullReturnsException()
+        {
+            var fakeUnityContainer = new UnityContainer();
+        }
+
+        [Fact]
+        public void DataAccessConfiguration_ConstructorParameterFileNameEmptyReturnsException()
+        {
+            var fakeUnityContainer = new UnityContainer();
+        }
+
+        [Fact]
+        public void DataAccessConfiguration_ConstructorParametersNotNullReturnsDataAccessConfiguration()
+        {
+            var fakeUnityContainer = new UnityContainer();
+        }
+    }
+}
---
+++ APUS.UnitTests/ReportGeneratorTests.cs
@@ -0,0 +1,63 @@
+﻿namespace APUS.UnitTests
+{
+    using APUS.DataLoader;
+    using APUS.OutputFormatters;
+    using APUS.ViewModels;
+    using System;
+    using Xunit;
+
+    public class ReportGeneratorTests
+    {
+        [Fact]
+        public void ReportGeneratorConstructor_ConstructorParameterDataLoaderNull_ReturnsException()
+        {
+            IDataLoader fakeDataLoader = null;
+            var fakeOfficerViewModel = NSubstitute.Substitute.For<IOfficerViewModelDataMapper>();
+            var fakeOutputFormatter = NSubstitute.Substitute.For<IOutputFormatter>();
+
+            Assert.Throws<ArgumentNullException>(() => new ReportGenerator(fakeDataLoader, fakeOfficerViewModel, fakeOutputFormatter));
+        }
+
+        [Fact]
+        public void ReportGeneratorConstructor_ConstructorParameterOfficerViewModelNull_ReturnsException()
+        {
+            var fakeDataLoader = NSubstitute.Substitute.For<IDataLoader>();
+            IOfficerViewModelDataMapper fakeOfficerViewModel = null;
+            var fakeOutputFormatter = NSubstitute.Substitute.For<IOutputFormatter>();
+
+            Assert.Throws<ArgumentNullException>(() => new ReportGenerator(fakeDataLoader, fakeOfficerViewModel, fakeOutputFormatter));
+        }
+
+        [Fact]
+        public void ReportGeneratorConstructor_ConstructorParameterOutputFormatterNull_ReturnsException()
+        {
+            var fakeDataLoader = NSubstitute.Substitute.For<IDataLoader>(); ;
+            var fakeOfficerViewModel = NSubstitute.Substitute.For<IOfficerViewModelDataMapper>(); ;
+            IOutputFormatter fakeOutputFormatter = null;
+
+            Assert.Throws<ArgumentNullException>(() => new ReportGenerator(fakeDataLoader, fakeOfficerViewModel, fakeOutputFormatter));
+        }
+
+        [Fact]
+        public void ReportGeneratorConstructor_AllConstructorParameterNull_ReturnsException()
+        {
+            IDataLoader fakeDataLoader = null;
+            IOfficerViewModelDataMapper fakeOfficerViewModel = null;
+            IOutputFormatter fakeOutputFormatter = null;
+
+            Assert.Throws<ArgumentNullException>(() => new ReportGenerator(fakeDataLoader, fakeOfficerViewModel, fakeOutputFormatter));
+        }
+
+        [Fact]
+        public void ReportGeneratorConstructor_AllConstructorParameterNotNull_ReturnsReportGenerator()
+        {
+            var fakeDataLoader = NSubstitute.Substitute.For<IDataLoader>(); ;
+            var fakeOfficerViewModel = NSubstitute.Substitute.For<IOfficerViewModelDataMapper>();
+            var fakeOutputFormatter = NSubstitute.Substitute.For<IOutputFormatter>();
+
+            var reportGenerator = new ReportGenerator(fakeDataLoader, fakeOfficerViewModel, fakeOutputFormatter);
+
+            Assert.NotNull(reportGenerator);
+        }
+    }
+}
