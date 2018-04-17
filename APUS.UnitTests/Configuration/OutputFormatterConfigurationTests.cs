3d2cf47cfd2809f0ccdc482b1892fb15a95ebb65
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-04-17 17:29:42 +0200

    OutputFormatter Configuration tests

---
+++ APUS.UnitTests/Configuration/OutputFormatterConfigurationTests.cs
@@ -0,0 +1,45 @@
+﻿namespace APUS.UnitTests.Configuration
+{
+    using APUS.Configuration;
+    using APUS.OutputFormatters;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Unity;
+    using Xunit;
+
+    public class OutputFormatterConfigurationTests
+    {
+        [Fact]
+        public void OutputFormatterConfiguration_ConstructorParameterContainerNullReturnsException()
+        {
+            IUnityContainer fakeUnityContainer = null;
+
+            Assert.Throws<ArgumentNullException>(() => new OutputFormatterConfiguration(fakeUnityContainer));
+        }
+
+        [Fact]
+        public void OutputFormatterConfiguration_ConstructorParameterContainerNotNullReturnsOutputFormatterConfiguration()
+        {
+            var fakeUnityContainer = new UnityContainer();
+
+            var outputFormatterConfiguration = new OutputFormatterConfiguration(fakeUnityContainer);
+
+            Assert.NotNull(outputFormatterConfiguration);
+        }
+
+        [Fact]
+        public void OutputFormatterConfiguration_ConfigureAddOutputFormatter()
+        {
+            var fakeUnityContainer = new UnityContainer();
+
+            var outputFormatterConfiguration = new OutputFormatterConfiguration(fakeUnityContainer);
+
+            outputFormatterConfiguration.Configure();
+
+            var isRegistered = fakeUnityContainer.IsRegistered<IOutputFormatter>();
+
+            Assert.True(isRegistered);
+        }
+    }
+}
