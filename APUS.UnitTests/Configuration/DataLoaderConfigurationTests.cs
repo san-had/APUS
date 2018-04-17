fc93c2fcadac8632abac0d381fe7f3f67a9fbf78
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-04-17 16:36:50 +0200

    DataLoader Configuration tests

---
+++ APUS.UnitTests/Configuration/DataLoaderConfigurationTests.cs
@@ -0,0 +1,57 @@
+﻿namespace APUS.UnitTests.Configuration
+{
+    using APUS.Configuration;
+    using APUS.DataLoader;
+    using System;
+    using Unity;
+    using Xunit;
+
+    public class DataLoaderConfigurationTests
+    {
+        [Fact]
+        public void DataLoaderConfiguration_ConstructorParameterContainerNullReturnsException()
+        {
+            IUnityContainer fakeUnityContainer = null;
+
+            Assert.Throws<ArgumentNullException>(() => new DataLoaderConfiguration(fakeUnityContainer));
+        }
+
+        [Fact]
+        public void DataLoaderConfiguration_ConstructorParameterContainerNotNullReturnsDataLoaderConfiguration()
+        {
+            var fakeUnityContainer = NSubstitute.Substitute.For<IUnityContainer>();
+
+            var dataLoaderConfiguration = new DataLoaderConfiguration(fakeUnityContainer);
+
+            Assert.NotNull(dataLoaderConfiguration);
+        }
+
+        [Fact]
+        public void DataLoaderConfiguration_ConfigureAddOfficerDataMapperToConfiguration()
+        {
+            var fakeUnityContainer = new UnityContainer();
+
+            var dataLoaderConfiguration = new DataLoaderConfiguration(fakeUnityContainer);
+
+            dataLoaderConfiguration.Configure();
+
+            var isRegistered = fakeUnityContainer.IsRegistered<IOfficerDataMapper>();
+
+            Assert.True(isRegistered);
+        }
+
+        [Fact]
+        public void DataLoaderConfiguration_ConfigureAddDataLoaderToConfiguration()
+        {
+            var fakeUnityContainer = new UnityContainer();
+
+            var dataLoaderConfiguration = new DataLoaderConfiguration(fakeUnityContainer);
+
+            dataLoaderConfiguration.Configure();
+
+            var isRegistered = fakeUnityContainer.IsRegistered<IDataLoader>();
+
+            Assert.True(isRegistered);
+        }
+    }
+}
