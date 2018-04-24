cd8d0ab17eb8d8c6b76b9548c2115cc119a80dcd
Sándor Hadházi <sandor.hadhazi@dealogic.com>
2018-04-24 13:25:27 +0200

    Added MenuConfiguration tests

---
+++ APUS.UnitTests/Configuration/MenuConfigurationTests.cs
@@ -0,0 +1,35 @@
+﻿namespace APUS.UnitTests.Configuration
+{
+    using APUS.Configuration;
+    using APUS.Utils;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Unity;
+    using Xunit;
+
+    public class MenuConfigurationTests
+    {
+        [Fact]
+        public void MenuConfiguration_ConstructorParameterContainerNullReturnsException()
+        {
+            IUnityContainer fakeUnityContainer = null;
+
+            Assert.Throws<ArgumentNullException>(() => new MenuConfiguration(fakeUnityContainer));
+        }
+
+        [Fact]
+        public void MenuConfiguration_ConstructorParameterContainerNotNullReturnsMenuConfiguration()
+        {
+            IUnityContainer fakeUnityContainer = new UnityContainer();
+
+            var menuConfiguration = new MenuConfiguration(fakeUnityContainer);
+
+            menuConfiguration.Configure();
+
+            var isRegistered = fakeUnityContainer.IsRegistered<IMenu>();
+
+            Assert.True(isRegistered);
+        }
+    }
+}
