79983ea0baee736d57f18a0e4872446a6c87c31d
san-had <hadhazis@gmail.com>
2018-05-06 07:10:26 +0200

    Completing DateParser tests

---
+++ APUS.UnitTests/DataLoader/UTCDateParserTests.cs
@@ -0,0 +1,50 @@
+﻿namespace APUS.UnitTests.DataLoader
+{
+    using System;
+    using Xunit;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using APUS.DataLoader;
+
+    public class UTCDateParserTests
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(null, "")]
+        [InlineData(null, "a-b-cT00:00:00Z")]
+        [InlineData(null, "2017-03--04T00:00:00KU")]
+        [InlineData(null, "2017-a-04T00:00:00Z")]
+        [InlineData(null, "2017-13-03T00:00:00Z")]
+        [InlineData(null, "2017-03-32T00:00:00Z")]
+        public void ParseUTCDateFormat_WrongStringFormat_ReturnsNull(DateTime? expectedDateTime, string dateTimeString)
+        {
+            var actualDate = new UTCDateTimeParser().ParseDate(dateTimeString);
+
+            Assert.Equal(expectedDateTime.HasValue, actualDate.HasValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetInputDataForParseUTCDateFormatTests))]
+        public void ParseUTCDateFormat_RightStringFormat_ReturnsDateTime(string inputString, DateTime? expectedDate)
+        {
+            var actualDate = new UTCDateTimeParser().ParseDate(inputString);
+
+            Assert.Equal(expectedDate.Value, actualDate.Value);
+        }
+
+        [ExcludeFromCodeCoverage]
+        public static IEnumerable<object[]> GetInputDataForParseUTCDateFormatTests()
+        {
+            DateTime? expedtedDate = new DateTime(2017, 4, 3);
+
+            var list = new List<object[]>()
+            {
+                new object[] { "2017-04-03T00:00:00Z", expedtedDate},
+                new object[] { "2017-4-3T50:00:00Z", expedtedDate},
+                new object[] { " 2017 - 04 - 03 T00:00:00Z ", expedtedDate},
+            };
+
+            return list;
+        }
+    }
+}
---
+++ APUS.UnitTests/DataLoader/UsDateParserTests.cs
@@ -0,0 +1,50 @@
+﻿namespace APUS.UnitTests.DataLoader
+{
+    using System;
+    using Xunit;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using APUS.DataLoader;
+
+    public class UsDateParserTests
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData(null, "")]
+        [InlineData(null, "a/b/c")]
+        [InlineData(null, "03\04/2017")]
+        [InlineData(null, "03/a/2017")]
+        [InlineData(null, "13/03/2017")]
+        [InlineData(null, "32/03/2017")]
+        public void ParseUsDateFormat_WrongStringFormat_ReturnsNull(DateTime? expectedDateTime, string dateTimeString)
+        {
+            var actualDate = new UsDateParser().ParseDate(dateTimeString);
+
+            Assert.Equal(expectedDateTime.HasValue, actualDate.HasValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetInputDataForParseUsDateFormatTests))]
+        public void ParseUsDateFormat_RightStringFormat_ReturnsDateTime(string inputString, DateTime? expectedDate)
+        {
+            var actualDate = new UsDateParser().ParseDate(inputString);
+
+            Assert.Equal(expectedDate.Value, actualDate.Value);
+        }
+
+        [ExcludeFromCodeCoverage]
+        public static IEnumerable<object[]> GetInputDataForParseUsDateFormatTests()
+        {
+            DateTime? expedtedDate = new DateTime(2017, 3, 4);
+
+            var list = new List<object[]>()
+            {
+                new object[] { "3/4/2017", expedtedDate},
+                new object[] { "03/04/2017", expedtedDate},
+                new object[] { " 03 / 04 / 2017 ", expedtedDate},
+            };
+
+            return list;
+        }
+    }
+}
